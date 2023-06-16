b388475cb6b1374b92d2772c7c6a9376f6420bb6
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-06-16 17:58:25 +0300

    initial commit

---
+++ spring-dto/src/test/java/com/ulascan/springdto/SpringDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulascan.springdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
